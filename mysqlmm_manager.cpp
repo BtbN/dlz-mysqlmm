244b4e8fffc6f3b58ad7174419567d1c1b7ee31a
BtbN <btbn@btbn.de>
2014-09-17 16:17:36 +0200

    Config parsing

--- mysqlmm_manager.cpp
+++ mysqlmm_manager.cpp
@@ -1,4 +1,5 @@
 #include <exception>
+#include <fstream>
 
 #include <json/json.h>
 #include <mysql_driver.h>
@@ -46,16 +47,13 @@ class mysqlmm_thread_init
 
 static thread_local mysqlmm_thread_init mysqlmm_thread_init_obj;
 
-MySQLMMManager *MySQLMMManager::inst = nullptr;
-
 MySQLMMManager::MySQLMMManager(const std::string& dlzname,
                                const bind9_functions& b9f,
                                const std::vector<std::string>& args)
 	:f(b9f)
+	,initial_connections(1)
+	,max_connections(1)
 {
-	if(inst)
-		throw std::runtime_error("Tried to create more than one MySQLMMManager");
-
 	if(args.size() != 2)
 		throw std::runtime_error("MySQLMM expects exactly one argument");
 
@@ -64,20 +62,70 @@ MySQLMMManager::MySQLMMManager(const std::string& dlzname,
 	driver = sql::mysql::get_driver_instance();
 
 	mysqlmm_thread_init_obj.init(this);
-	inst = this;
 }
 
 MySQLMMManager::~MySQLMMManager()
 {
-	inst = nullptr;
+}
+
+void MySQLMMManager::readConfig(const std::string &cfg)
+{
+	try
+	{
+		Json::Reader reader;
+		Json::Value root;
+
+		std::ifstream ifs;
+		ifs.open(cfg);
+
+		if(!reader.parse(ifs, root))
+			throw std::runtime_error("Failed parsing MySQLMM config");
+
+		url = root.get("url", "tcp://localhost/bind").asString();
+		user = root.get("user", "bind").asString();
+		password = root.get("pass", "").asString();
+		db = root.get("db", "").asString();
+		initial_connections = root.get("initial_connections", 1).asUInt();
+		max_connections = root.get("max_connections", initial_connections + 8).asUInt();
+
+		Json::Value queriesObj = root["queries"];
+
+		for(const std::string &queryname: queriesObj.getMemberNames())
+		{
+			Json::Value queryval = queriesObj[queryname];
+
+			mmquery query;
+
+			query.sql = queryval.get("sql", "").asString();
+
+			Json::Value paramsArr = queryval["params"];
+
+			if(paramsArr.isArray())
+			{
+				for(const Json::Value &param: paramsArr)
+				{
+					query.params.push_back(param.asString());
+				}
+			}
+
+			queries[queryname] = std::move(query);
+		}
+	}
+	catch(const std::exception &e)
+	{
+		std::string msg = "Failed reading MySQLMM configuration: ";
+		throw std::runtime_error(msg + e.what());
+	}
 }
 
 std::shared_ptr<sql::Connection> MySQLMMManager::spawnConnection()
 {
-
+	std::shared_ptr<sql::Connection> res;
+	return res;
 }
 
 std::shared_ptr<sql::Connection> MySQLMMManager::getFreeConnection()
 {
-
+	std::shared_ptr<sql::Connection> res;
+	return res;
 }
--- mysqlmm_manager.h
+++ mysqlmm_manager.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <unordered_map>
 
 #include "util.h"
 
@@ -13,16 +14,28 @@ namespace sql
 		class MySQL_Driver;
 	}
 	class Connection;
+	class PreparedStatement;
 }
 
 class MySQLMMManager
 {
 	friend class mysqlmm_thread_init;
 
+	struct mmquery
+	{
+		std::string sql;
+		std::vector<std::string> params;
+		std::shared_ptr<sql::PreparedStatement> prep_stmt;
+	};
+
+	struct mmconn
+	{
+		std::shared_ptr<sql::Connection> connection;
+		std::unordered_map<std::string, mmquery> queries;
+	};
+
 	MySQLMMManager(const MySQLMMManager&) = delete;
 	MySQLMMManager& operator=(const MySQLMMManager&) = delete;
-
-	static MySQLMMManager *inst;
 
 	public:
 	MySQLMMManager(const std::string &dlzname,
@@ -31,11 +44,20 @@ class MySQLMMManager
 	~MySQLMMManager();
 
 	private:
+	void readConfig(const std::string &cfg);
 	std::shared_ptr<sql::Connection> spawnConnection();
 	std::shared_ptr<sql::Connection> getFreeConnection();
 
 	private:
 	sql::mysql::MySQL_Driver *driver;
-	std::vector<std::shared_ptr<sql::Connection>> connections;
+	std::vector<std::shared_ptr<mmconn>> connections;
 	bind9_functions f;
+
+	std::string url;
+	std::string user;
+	std::string password;
+	std::string db;
+	unsigned int initial_connections;
+	unsigned int max_connections;
+	std::unordered_map<std::string, mmquery> queries;
 };
